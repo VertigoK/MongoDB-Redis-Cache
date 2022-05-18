cb695aa635ea3312214ff4550f0cb95a458ce8cb
Kyungeun <ke.kevin.kim@gmail.com>
2022-05-18 16:48:50 +0900

    Update MongoDbRedisCacheApplication.java

--- src/main/java/kevin/test/mongodbrediscache/MongoDbRedisCacheApplication.java
+++ src/main/java/kevin/test/mongodbrediscache/MongoDbRedisCacheApplication.java
@@ -2,10 +2,8 @@ package kevin.test.mongodbrediscache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class MongoDbRedisCacheApplication {
 
     public static void main(String[] args) {
